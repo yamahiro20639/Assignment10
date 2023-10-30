9dd56c95a0521cf2bd03e6ef91a36f396bd46dec
yamahiro20639 <144509349+yamahiro20639@users.noreply.github.com>
2023-10-31 06:32:32 +0900

    重複登録の例外処理テスト

--- src/test/java/movieinformation/service/MovieInformationServiceTest.java
+++ src/test/java/movieinformation/service/MovieInformationServiceTest.java
@@ -1,5 +1,6 @@
 package movieinformation.service;
 
+import movieinformation.Exception.MovieDuplicationException;
 import movieinformation.Exception.MovieInformationNotFoundException;
 import movieinformation.entity.Movie;
 import movieinformation.mapper.MovieInformationMapper;
@@ -74,5 +75,12 @@ class MovieInformationServiceTest {
         doNothing().when(movieInformationMapper).insertMovie(movie);
         Movie actual = movieInformationService.insertMovie(movie);
         assertThat(actual).isEqualTo(new Movie("Marvel's The Avengers", LocalDate.of(2012, 8, 14), "Joseph Hill Whedon", 1518812988));
+    }
+
+    @Test
+    public void 存在する映画情報を新規登録する場合に重複登録の例外処理が動作すること() throws MovieDuplicationException {
+        Movie movie = new Movie("Episode IV – A New Hope", LocalDate.of(1978, 6, 30), "George Walton Lucas Jr.", 775398007);
+        doThrow(new MovieDuplicationException("Already registered data")).when(movieInformationMapper).insertMovie(movie);
+        assertThrows(MovieDuplicationException.class, () -> movieInformationService.insertMovie(new Movie("Episode IV – A New Hope", LocalDate.of(1978, 6, 30), "George Walton Lucas Jr.", 775398007)));
     }
 }
