36e47586e210af85fe9b412a57218b33302a72f8
yamahiro20639 <144509349+yamahiro20639@users.noreply.github.com>
2023-11-05 19:31:01 +0900

    POST機能のDBテスト実装

--- src/test/java/movieinformation/mapper/MovieInformationMapperTest.java
+++ src/test/java/movieinformation/mapper/MovieInformationMapperTest.java
@@ -1,5 +1,8 @@
 package movieinformation.mapper;
 
+import com.github.database.rider.core.api.dataset.DataSet;
+import com.github.database.rider.core.api.dataset.ExpectedDataSet;
+import com.github.database.rider.spring.api.DBRider;
 import movieinformation.entity.Movie;
 import org.junit.jupiter.api.Test;
 import org.mybatis.spring.boot.test.autoconfigure.MybatisTest;
@@ -18,6 +21,7 @@ import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@DBRider
 @MybatisTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class MovieInformationMapperTest {
@@ -64,5 +68,32 @@ class MovieInformationMapperTest {
         Optional<Movie> movie = movieInformationMapper.findById(4);
         assertThat(movie).isEmpty();
     }
-    
+
+    //POSTのDBテスト
+    @Test
+    @Sql(
+            scripts = {"classpath:/databases/delete-movies.sql", "classpath:/databases/insert-movies.sql"},
+            executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD
+    )
+    @Transactional
+    void 新規の映画情報を登録することver1() {
+        Movie movie = new Movie("Episode I – The Phantom Menace", LocalDate.of(1997, 7, 10), "George Walton Lucas Jr.", 1027082707);
+        movieInformationMapper.insert(movie);
+        List<Movie> movies = movieInformationMapper.findAll();
+        assertThat(movies).hasSize(4)
+                .contains(
+                        new Movie(1, "Episode IV – A New Hope", LocalDate.of(1978, 6, 30), "George Walton Lucas Jr.", 775398007),
+                        new Movie(2, "Episode V – The Empire Strikes Back", LocalDate.of(1980, 6, 28), "Irvin Kershner", 538375067),
+                        new Movie(3, "Episode VI – Return of the Jedi", LocalDate.of(1983, 7, 2), "Richard Marquand", 475106177),
+                        movie
+                );
+    }
+
+    @Test
+    @ExpectedDataSet(value = "insert_movieData.yml")
+    @Transactional
+    void 新規の映画情報を登録することver2() {
+        movieInformationMapper.insert(new Movie("Episode I – The Phantom Menace", LocalDate.of(1999, 7, 10), "George Walton Lucas Jr.", 1027082707));
+    }
+
 }
