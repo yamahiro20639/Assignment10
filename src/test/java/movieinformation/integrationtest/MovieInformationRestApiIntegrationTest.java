535f13423e1c2b8f4ffac67e7829dd3e89afc40d
yamahiro20639 <144509349+yamahiro20639@users.noreply.github.com>
2023-11-13 21:33:58 +0900

    Read機能の全件取得に対する結合テストコードをを実装

--- src/test/java/movieinformation/integrationtest/MovieInformationRestApiIntegrationTest.java
+++ src/test/java/movieinformation/integrationtest/MovieInformationRestApiIntegrationTest.java
@@ -8,7 +8,10 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDate;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -22,7 +25,11 @@ public class MovieInformationRestApiIntegrationTest {
     @Transactional
     void 映画情報を全件取得すること() throws Exception{
         mockMvc.perform(MockMvcRequestBuilders.get("/movies"))
-                .andExpect(new Movie());
+                .andExpect(MockMvcResultMatchers.status().isOk());
     }
 
 }
+
+//new Movie(1,"Episode I – The Phantom Menace", LocalDate.of(1999,7,10),"George Walton Lucas Jr.",1027082707),
+        //new Movie(2,"Episode II – Attack of the Clones", LocalDate.of(2002,5,16),"George Walton Lucas Jr.",653779970),
+        //new Movie(3,"Episode III – Revenge of the Sith", LocalDate.of(2005,7,9),"George Walton Lucas Jr.",868390560)
