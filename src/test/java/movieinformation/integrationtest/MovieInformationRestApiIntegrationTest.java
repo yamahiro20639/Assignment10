6700c957e0f78058c2f9a42fc4ae4522d2c6897b
yamahiro20639 <144509349+yamahiro20639@users.noreply.github.com>
2023-11-13 23:33:12 +0900

    Read機能の全件取得に対する結合テストコードについて拡張実装

--- src/test/java/movieinformation/integrationtest/MovieInformationRestApiIntegrationTest.java
+++ src/test/java/movieinformation/integrationtest/MovieInformationRestApiIntegrationTest.java
@@ -1,8 +1,12 @@
 package movieinformation.integrationtest;
 
 import com.github.database.rider.core.api.dataset.DataSet;
+import com.github.database.rider.spring.api.DBRider;
 import org.junit.jupiter.api.Test;
+import org.skyscreamer.jsonassert.JSONAssert;
+import org.skyscreamer.jsonassert.JSONCompareMode;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
@@ -10,9 +14,13 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.nio.charset.StandardCharsets;
+
 @SpringBootTest
 @AutoConfigureMockMvc
-public class MovieInformationRestApiIntegrationTest {
+@DBRider
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+ class MovieInformationRestApiIntegrationTest {
     @Autowired
     MockMvc mockMvc;
 
@@ -20,9 +28,35 @@ public class MovieInformationRestApiIntegrationTest {
     @Test
     @DataSet(value = "datasets/movieData.yml")
     @Transactional
-    void 映画情報を全件取得すること() throws Exception{
-        mockMvc.perform(MockMvcRequestBuilders.get("/movies"))
-                .andExpect(MockMvcResultMatchers.status().isOk());
+    public void 映画情報をステータスコード200で全件取得すること() throws Exception{
+        String response = mockMvc.perform(MockMvcRequestBuilders.get("/movies"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andReturn().getResponse().getContentAsString(StandardCharsets.UTF_8);
+        JSONAssert.assertEquals("""
+                [
+                 {
+                   "id": 1,
+                   "name": "Episode I – The Phantom Menace",
+                   "release_date": "1999-07-10",
+                   "director_name": "George Walton Lucas Jr",
+                   "box_office": 1027082707
+                 },
+                 {
+                   "id": 2,
+                   "name": "Episode II – Attack of the Clones",
+                   "release_date": "2002-05-16",
+                   "director_name": "George Walton Lucas Jr",
+                   "box_office": 653779970
+                 },
+                 {
+                   "id": 3,
+                   "name": "Episode III – Revenge of the Sith",
+                   "release_date": "2005-07-09",
+                   "director_name": "George Walton Lucas Jr",
+                   "box_office": 868390560
+                 }
+                ]
+                 """,response, JSONCompareMode.STRICT);
     }
 
 }
