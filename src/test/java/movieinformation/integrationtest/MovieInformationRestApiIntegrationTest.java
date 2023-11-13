0ed7e56e2eeea61d1553894e6c31d9e68894c7c0
yamahiro20639 <144509349+yamahiro20639@users.noreply.github.com>
2023-11-13 21:44:11 +0900

    無駄なコードを削除

--- src/test/java/movieinformation/integrationtest/MovieInformationRestApiIntegrationTest.java
+++ src/test/java/movieinformation/integrationtest/MovieInformationRestApiIntegrationTest.java
@@ -1,7 +1,6 @@
 package movieinformation.integrationtest;
 
 import com.github.database.rider.core.api.dataset.DataSet;
-import movieinformation.entity.Movie;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -10,8 +9,6 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.time.LocalDate;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -29,7 +26,3 @@ public class MovieInformationRestApiIntegrationTest {
     }
 
 }
-
-//new Movie(1,"Episode I – The Phantom Menace", LocalDate.of(1999,7,10),"George Walton Lucas Jr.",1027082707),
-        //new Movie(2,"Episode II – Attack of the Clones", LocalDate.of(2002,5,16),"George Walton Lucas Jr.",653779970),
-        //new Movie(3,"Episode III – Revenge of the Sith", LocalDate.of(2005,7,9),"George Walton Lucas Jr.",868390560)
