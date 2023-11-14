39824e15325d5019a1bfaa4fae57c3c60057f70a
yamahiro20639 <144509349+yamahiro20639@users.noreply.github.com>
2023-11-14 19:03:14 +0900

    Read機能の全件取得に対する結合テストコード修正

--- src/test/java/movieinformation/integrationtest/MovieInformationRestApiIntegrationTest.java
+++ src/test/java/movieinformation/integrationtest/MovieInformationRestApiIntegrationTest.java
@@ -37,23 +37,23 @@ import java.nio.charset.StandardCharsets;
                  {
                    "id": 1,
                    "name": "Episode I – The Phantom Menace",
-                   "release_date": "1999-07-10",
-                   "director_name": "George Walton Lucas Jr",
-                   "box_office": 1027082707
+                   "releaseDate": "1999-07-10",
+                   "directorName": "George Walton Lucas Jr.",
+                   "boxOffice": 1027082707
                  },
                  {
                    "id": 2,
                    "name": "Episode II – Attack of the Clones",
-                   "release_date": "2002-05-16",
-                   "director_name": "George Walton Lucas Jr",
-                   "box_office": 653779970
+                   "releaseDate": "2002-05-16",
+                   "directorName": "George Walton Lucas Jr.",
+                   "boxOffice": 653779970
                  },
                  {
                    "id": 3,
                    "name": "Episode III – Revenge of the Sith",
-                   "release_date": "2005-07-09",
-                   "director_name": "George Walton Lucas Jr",
-                   "box_office": 868390560
+                   "releaseDate": "2005-07-09",
+                   "directorName": "George Walton Lucas Jr.",
+                   "boxOffice": 868390560
                  }
                 ]
                  """,response, JSONCompareMode.STRICT);
