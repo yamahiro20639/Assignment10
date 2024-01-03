1ae09ab013cf9b49eb0237d75ac0608486036529
yamahiro20639 <144509349+yamahiro20639@users.noreply.github.com>
2024-01-03 15:54:38 +0900

    不必要なimportを削除

--- src/test/java/movieinformation/integrationtest/MovieInformationRestApiIntegrationTest.java
+++ src/test/java/movieinformation/integrationtest/MovieInformationRestApiIntegrationTest.java
@@ -16,7 +16,6 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.transaction.annotation.Transactional;
